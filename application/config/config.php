b8d6f419078b37b2b8f55cf93825cf305f770b5a
Crescent <hello@crescentke.com>
2017-09-07 22:51:21 +0300

    Project

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://139.162.133.122';
+$config['base_url'] = 'http://139.162.133.122/bflogin';
 
 /*
 |--------------------------------------------------------------------------
